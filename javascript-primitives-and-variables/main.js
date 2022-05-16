ddb23157f8a959125d39a94a0ea51e38e57d358a
Nolan Reda <nolan.reda@gmail.com>
2022-05-16 17:29:33 +0000

    completed variables exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Nolan Reda';
+var isCool = true;
+var totalPets = 0;
+
+console.log('My Name:', fullName);
+console.log('Am I cool?', isCool);
+console.log('how many pets do I have:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+var unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);

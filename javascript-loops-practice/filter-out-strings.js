f8115e80d988a241906786e2d292116210598437
Nolan Reda <nolan.reda@gmail.com>
2022-05-20 22:46:48 +0000

    changes

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -2,7 +2,7 @@
 function filterOutStrings(values) {
   var noStrings = [];
   for (let i = 0; i < values.length; i++) {
-    if (typeof values[i] === 'number') {
+    if (typeof values[i] !== 'string') {
       noStrings.push(values[i]);
     }
   } return noStrings;

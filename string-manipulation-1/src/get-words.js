f0c0e4de0856f443c4da05f0d923cda3751d82a5
Nolan Reda <nolan.reda@gmail.com>
2022-05-20 22:44:44 +0000

    just for tracking

---
+++ string-manipulation-1/src/get-words.js
@@ -0,0 +1,10 @@
+/* exported getWords */
+
+// take each value separated by spaces from a string value
+// convert them over to a new array
+// return new array
+
+function getWords(string) {
+  var words = [];
+  return words.push(string.slice(' '));
+}
---
+++ string-manipulation-1/src/reverse-word.js
@@ -0,0 +1,16 @@
+/* exported reverseWord */
+
+// function reverseWord(string) {
+//   debugger;
+//   var reverse = [];
+//   for (let i = string.length; i > string[i]; i--) {
+//     reverse.push(string.charAt(i));
+//   }
+//   return reverse;
+// }
+
+// console.log(reverseWord('foo'));
+
+// function reverseWord(string) {
+//   if
+// }

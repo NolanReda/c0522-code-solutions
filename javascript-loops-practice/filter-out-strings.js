802fcced02bcf38b19d53d899a0d00ef726285c2
Nolan Reda <nolan.reda@gmail.com>
2022-05-20 17:02:09 +0000

    completed loops practice

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -1,1 +1,9 @@
 /* exported filterOutStrings */
+function filterOutStrings(values) {
+  var noStrings = [];
+  for (let i = 0; i < values.length; i++) {
+    if (typeof values[i] === 'number') {
+      noStrings.push(values[i]);
+    }
+  } return noStrings;
+}

2cadd24544e6657f1d2414fcf04970b4f24345c4
Nolan Reda <nolan.reda@gmail.com>
2022-05-21 18:11:01 +0000

    just changes

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,10 @@
+/* exported getValue */
+
+// take the value of properties from an object
+//
+
+// const dave = { firstName: 'David', lastName: 'Thomas' };
+
+// function getValue(object, key) {
+
+// console.log(getValue(dave, 'firstname'));

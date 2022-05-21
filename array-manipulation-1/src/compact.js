70e28bc2ac7974097bac0413b0efe17b8203a58c
Nolan Reda <nolan.reda@gmail.com>
2022-05-21 22:55:36 +0000

    just changes

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,4 +1,11 @@
 /* exported compact */
+
+// create a new array to show included values
+// search each value in a given arary
+// find values other than the ommited value to be included in the new array
+// skip over ommited values
+// give back the new array out of the funciton
+
 function compact(array, value) {
   var newArray = [];
   for (let i = 0; i < array.length; i++) {
@@ -7,3 +14,4 @@ function compact(array, value) {
     }
   } return newArray;
 }
+// console.log(compact([{}, null, {}, {}], null));

247292df0767fd845993563e9bed3f3f68535988
Nolan Reda <nolan.reda@gmail.com>
2022-05-22 00:30:45 +0000

    nothing again

--- object-manipulation-1/src/get-value.js
+++ object-manipulation-1/src/get-value.js
@@ -1,17 +1,8 @@
 // /* exported getValue */
 
-// // take the value of properties from an object
-// //
+// find values of each object property
+// return the value of each one
 
-// const dave = { firstName: 'David', lastName: 'Thomas' };
-
-// function getValue(object, key) {
-//   var values = '';
-//   for (const value in object[key]) {
-//     if (object[value]) {
-//       values = values + object[key][value];
-//     }
-//   } return values;
-// }
-
-// // console.log(getValue(dave, 'firstname'));
+function getValue(object, key) {
+  return object[key];
+} getValue();

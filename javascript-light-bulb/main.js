fc6611e116b55eafb8efce4a208959ce1ab85f08
Nolan Reda <nolan.reda@gmail.com>
2022-05-24 21:42:49 +0000

    added psuedo-code

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,3 +1,9 @@
+// need to query select from the elements that will be effected by the changing light effect(div with bulb in it and background color of body)
+// create a new variable with boolean value to serve as a "lightswitch"
+// create a new function to test which state the boolean is in and edit the class values of
+// the effected HTML elements to match the CSS selectors with the correct color rules applied to them.
+// add an event listener to the 'lightbulb' to activate the new function when clicked
+
 var $body = document.querySelector('body');
 
 var $bulb = document.querySelector('.light-bulb');

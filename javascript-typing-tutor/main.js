fa51ec178da23e4a5cb13c33b3ac43be2035ff2c
Nolan Reda <nolan.reda@gmail.com>
2022-05-25 17:07:09 +0000

    changes

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -2,13 +2,11 @@ var $letters = document.querySelectorAll('span');
 
 function keyPress(event) {
   for (let i = 0; i < $letters.length; i++) {
-    if (KeyboardEvent === $letters[i]) {
-      $letters[i].className = 'green';
-      $letters[i + 1].className = 'border-bottom';
-    }
+    $letters[i].className = 'green';
+    $letters[i + 1].className = 'border-bottom';
   }
 }
 
-$letters.addEventListener('keydown', keyPress);
+document.addEventListener('keydown', keyPress);
 
 // console.log(keyPress());

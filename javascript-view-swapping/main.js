4e021951c83f8774ab8dccdd3b5255dd908c7f30
Nolan Reda <nolan.reda@gmail.com>
2022-05-27 03:20:34 +0000

    completed view swapping exercise

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,27 @@
+var $tabContainer = document.querySelector('.tab-container');
+var $allTabs = document.querySelectorAll('.tab');
+var $allView = document.querySelectorAll('.view');
+
+function handleClick(event) {
+  if (event.target.matches('.tab')) {
+    for (let i = 0; i < $allTabs.length; i++) {
+      if ($allTabs[i] === event.target) {
+        $allTabs[i].className = 'tab active';
+      } else if ($allTabs[i] !== event.target.className) {
+        $allTabs[i].className = 'tab';
+      }
+      if (event.target.matches('.tab')) {
+        var $dataView = event.target.getAttribute('data-view');
+        for (let e = 0; e < $allView.length; e++) {
+          if ($allView[e].getAttribute('data-view') === event.target.getAttribute('data-view')) {
+            $allView[e].className = 'view';
+          } else if ($allView[e].getAttribute($dataView) !== event.target.$dataView) {
+            $allView[e].className = 'view hidden';
+          }
+        }
+      }
+    }
+  }
+}
+
+$tabContainer.addEventListener('click', handleClick);

1c1b5b6f2aaa385d413f29d4086c8ff55d3172f3
Nolan Reda <nolan.reda@gmail.com>
2022-05-29 01:37:49 +0000

    changes

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -2,7 +2,7 @@
 
 function lastChars(length, string) {
   if (string.length >= length) {
-    return string.slice(length, string.length);
+    return string.slice(-length, string.length);
   } else {
     return string;
   }

b1c94388ded420b97732df44b290193a123e31b0
Nolan Reda <nolan.reda@gmail.com>
2022-06-06 18:01:08 +0000

    requested changes

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -7,7 +7,7 @@ condition to deal with the word "JavaScript" specifically
 function capitalizeWord(string) {
   if (string.toLowerCase() === 'javascript') {
     return 'JavaScript';
-  } else if (string.charAt(0)) {
+  } else {
     return string.charAt(0).toUpperCase() + string.substring(1, string.length).toLowerCase();
   }
 }

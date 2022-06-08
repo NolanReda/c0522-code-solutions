0ae6eeb4cc1d171c96cf9c795c418b6df6374960
Nolan Reda <nolan.reda@gmail.com>
2022-06-08 19:03:00 +0000

    whole song

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -12,3 +12,23 @@ function interval() {
 }
 
 var timerID = setInterval(interval, 775);
+
+function lyr1() {
+  $h1.innerHTML = '~Driiiiiiftiiiiing, faaaaaalliiiing~';
+}
+setTimeout(lyr1, 6100);
+
+function lyr2() {
+  $h1.innerHTML = '~Floooatiiiing Weeeeiiiightleeeess~';
+}
+setTimeout(lyr2, 8425);
+
+function lyr3() {
+  $h1.innerHTML = '~Caaaaaalliiing, caaaaaalliiing hoooooooomme~';
+}
+setTimeout(lyr3, 10750);
+
+function lyr4() {
+  $h1.innerHTML = '&#119136';
+}
+setTimeout(lyr4, 14000);

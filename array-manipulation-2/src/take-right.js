89f3fe4b911470374f188a69fc875667c804bc0c
Nolan Reda <nolan.reda@gmail.com>
2022-06-09 04:31:47 +0000

    some changes

--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,13 +1,19 @@
 /* exported takeRight */
 
 function takeRight(array, count) {
-  if (array.length > count) {
-    var newArray = [];
-    for (let i = array.length; i > count - 1; i--) {
-      newArray.push(array[i]);
-    }
-    return newArray.reverse();
-  } else {
-    return array;
+  var newArray = [];
+  for (let i = array.length - 1; i > count - 1; i--) {
+    newArray.push(array[i]);
   }
+  return newArray.reverse();
 }
+
+// if (array.length > count) {
+//   var newArray = [];
+//   for (let i = array.length; i > count - 1; i--) {
+//     newArray.push(array[i]);
+//   }
+//   return newArray.reverse();
+// } else {
+//   return array;
+// }

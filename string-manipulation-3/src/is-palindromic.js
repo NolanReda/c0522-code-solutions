20be6c908b8a5a033bc49f4a4b6d854ded8d82e3
Nolan Reda <nolan.reda@gmail.com>
2022-06-10 00:59:59 +0000

    start

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,1 +1,11 @@
 /* exported isPalindromic */
+
+function isPalindromic(string) {
+  var arr = string.split('');
+  var arrReverse = arr.reverse().join('');
+  if (string === arrReverse) {
+    return true;
+  } else {
+    return false;
+  }
+}

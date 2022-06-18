539596a096bfd89524c8706ba60395d9a03e92e3
Nolan Reda <nolan.reda@gmail.com>
2022-06-18 00:35:04 +0000

    changes at the end of friday

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,13 +1,31 @@
 /* exported isAnagram */
 
 function isAnagram(firstString, secondString) {
-  for (let i = 0; i < firstString.length; i++) {
-    if (i === firstString.length - 1) {
+  var newFirst = firstString.split(' ').join('');
+  var newSecond = secondString.split(' ').join('');
+  for (let i = 0; i < newSecond.length; i++) {
+    if (i === newSecond.length - 1) {
       return true;
-    } else if (!secondString.includes(firstString[i])) {
+    } else if (!newFirst.includes(newSecond[i])) {
       return false;
-    } else if (secondString.includes(firstString[i])) {
-      continue;
+    } else if (newFirst.includes(newSecond[i])) {
+      var first = 0;
+      for (let f = 0; f < newFirst.length; f++) {
+        if (newFirst[f] === newSecond[i]) {
+          first++;
+        }
+      }
+      var second = 0;
+      for (let s = 0; s < newSecond.length; s++) {
+        if (newSecond[s] === newSecond[i]) {
+          second++;
+        }
+      }
+      if (first === second) {
+        return true;
+      } else if (first !== second) {
+        return false;
+      }
     }
   }
 }
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,28 @@
 /* exported titleCase */
+
+function titleCase(title) {
+  var split = title.split(' ');
+  var capArray = [];
+  for (let i = 0; i < split.length; i++) {
+    var cap = split[i].charAt(0).toUpperCase() + split[i].substring(1, split[i].length).toLowerCase();
+    if (split[i].toLowerCase() === 'javascript') {
+      cap = 'JavaScript';
+    }
+    if (split[i].toLowerCase() === 'api') {
+      cap = 'API';
+    }
+    if (split[i].toLowerCase() === 'and' || split[i].toLowerCase() === 'or' ||
+       split[i].toLowerCase() === 'to' || split[i].toLowerCase() === 'the' || split[i].toLowerCase() === 'of' ||
+         split[i].toLowerCase() === 'for' || split[i].toLowerCase() === 'an' ||
+         split[i].toLowerCase() === 'in' || split[i].toLowerCase() === 'on') {
+      cap = split[i].toLowerCase();
+    }
+    capArray.push(cap);
+    // console.log(capArray);
+    for (let p = 0; p < capArray.length; p++) {
+      if (capArray[p].includes(':')) {
+        // console.log(typeof capArray[p + 1]);
+      }
+    }
+  }
+}

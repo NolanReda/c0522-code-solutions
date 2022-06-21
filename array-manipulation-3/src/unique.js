49258a42a1a4fb26c1d2d6e2681578540df4a438
Nolan Reda <nolan.reda@gmail.com>
2022-06-21 22:27:44 +0000

    finished unique

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,15 @@
 /* exported unique */
+
+// create a new array to hold unique instances from old array
+// loop through old array and check if the index doesn't exist in the new array
+// if not, add it to the new array
+
+function unique(array) {
+  var newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!newArr.includes(array[i])) {
+      newArr.push(array[i]);
+    }
+  }
+  return newArr;
+}

48241a3b6e48f0b2e8799b0a0b03464cf327e77e
Nolan Reda <nolan.reda@gmail.com>
2022-06-21 22:14:43 +0000

    waiting on review for a bug

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,28 +1,42 @@
 /* exported titleCase */
 
 function titleCase(title) {
-  var split = title.split(' ');
   var capArray = [];
+  var split = title.split(' ');
   for (let i = 0; i < split.length; i++) {
     var cap = split[i].charAt(0).toUpperCase() + split[i].substring(1, split[i].length).toLowerCase();
     if (split[i].toLowerCase() === 'javascript') {
       cap = 'JavaScript';
+    } else if (split[i].toLowerCase() === 'javascript:') {
+      cap = 'JavaScript:';
     }
     if (split[i].toLowerCase() === 'api') {
       cap = 'API';
     }
     if (split[i].toLowerCase() === 'and' || split[i].toLowerCase() === 'or' ||
-       split[i].toLowerCase() === 'to' || split[i].toLowerCase() === 'the' || split[i].toLowerCase() === 'of' ||
-         split[i].toLowerCase() === 'for' || split[i].toLowerCase() === 'an' ||
-         split[i].toLowerCase() === 'in' || split[i].toLowerCase() === 'on') {
+      split[i].toLowerCase() === 'to' || split[i].toLowerCase() === 'the' || split[i].toLowerCase() === 'of' ||
+      split[i].toLowerCase() === 'for' || split[i].toLowerCase() === 'an' ||
+      split[i].toLowerCase() === 'in' || split[i].toLowerCase() === 'on') {
       cap = split[i].toLowerCase();
     }
+    if (i === 0) {
+      cap = split[i].charAt(0).toUpperCase() + split[i].substring(1, split[i].length).toLowerCase();
+    }
     capArray.push(cap);
-    var colon = capArray.join(' ').split(':');
-    // console.log(colon[1]);
-    var afterColon = colon[1].charAt(0).toUpperCase() + colon[1].substring(1, colon[1].length.toLowerCase());
-    afterColon.join(': ');
-    // console.log(afterColon);
+    var newString = capArray.join(' ');
+    var strArr = newString.split('');
+    var nextArr = [];
+    for (let s = 0; s < strArr.length; s++) {
+      var cap2 = strArr[s];
+      if (strArr[s - 1] === '-') {
+        cap2 = strArr[s].toUpperCase();
+      }
+      if (strArr[s - 2] === ':') {
+        cap2 = strArr[s].toUpperCase();
+      }
+      nextArr.push(cap2);
+    }
+    var anotherStr = nextArr.join('');
   }
-  return afterColon;
+  return anotherStr;
 }

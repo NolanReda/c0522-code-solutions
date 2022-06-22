0959f6e5cb18e5e8241a0df51269c20252275900
Nolan Reda <nolan.reda@gmail.com>
2022-06-22 22:11:24 +0000

    completed arrays 3

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,26 @@
 /* exported zip */
+
+// create a new array to hold changed data from input array
+// create a counter var to track the first array
+// loop the second array
+// if one array reaches it's length, return the first created array
+// as each pair of index's is accessed create a new array and store them in first-second order
+// add each new array to the first created array
+
+function zip(first, second) {
+  var newArr = [];
+  var f = 0;
+  for (let i = 0; i < second.length; i++) {
+    if (f === first.length || i === second.length) {
+      return newArr;
+    }
+    var yes = [];
+    if (i === f) {
+      yes.push(first[f]);
+      yes.push(second[i]);
+      newArr.push(yes);
+      f++;
+    }
+  }
+  return newArr;
+}

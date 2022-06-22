4f95c62e3054d4c48257cf885f2228b5150e1426
Nolan Reda <nolan.reda@gmail.com>
2022-06-22 00:18:13 +0000

    finished union

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,22 @@
 /* exported union */
+
+// create a new array to hold the desired values of both parameter arrays
+// loop through the first array
+// if each value does not yet appear in the new array, add it
+// if it does exist in the new array, skip it
+// do the same for the second array
+
+function union(first, second) {
+  var newArr = [];
+  for (let f = 0; f < first.length; f++) {
+    if (!newArr.includes(first[f])) {
+      newArr.push(first[f]);
+    }
+  }
+  for (let s = 0; s < second.length; s++) {
+    if (!newArr.includes(second[s])) {
+      newArr.push(second[s]);
+    }
+  }
+  return newArr;
+}

80cc94c7fc027e8469c5d6cd129d012695e7096c
Nolan Reda <nolan.reda@gmail.com>
2022-06-22 20:00:37 +0000

    changed equal psuedo-code

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,14 +1,15 @@
 /* exported equal */
 
+// create a counter to track the first array position in the first array
 // loop through both arrays
 // if each one does not match when the loop counter is in the same place, return false
+// if they do match, increment the counter for the first array and continue the loop
 // if the end of the array is reached, return true
 // if the 2 arrays are not the same length, return false
 // if the arrays are empty, return true
-// otherwise return true
 
 function equal(first, second) {
-  var f = 0;
+  let f = 0;
   for (let s = 0; s < second.length; s++) {
     if (f === first.length - 1 && first[first.length === second[second.length]]) {
       return true;

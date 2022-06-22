3b6a71497ce7ed4f409486a89ef79d2871c68869
Nolan Reda <nolan.reda@gmail.com>
2022-06-22 00:53:12 +0000

    almost done with equal

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,1 +1,18 @@
 /* exported equal */
+
+// loop through both arrays
+// if each one does not match when the loop counter is in the same place, return false
+// otherwise return true
+
+function equal(first, second) {
+  var f = 0;
+  for (let s = 0; s < second.length; s++) {
+    if (f === first.length - 1) {
+      return true;
+    } else if (first[f] === second[s]) {
+      f++;
+    } else if (first[f] !== second[s]) {
+      return false;
+    }
+  }
+}

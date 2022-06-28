94c53bca5a071a68959d131a8c9337b3d0d7c42b
Nolan Reda <nolan.reda@gmail.com>
2022-06-28 19:26:45 +0000

    created makeDeck function with loop

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -28,13 +28,6 @@ function makeDeck() {
   }
 }
 
-// var deck = [
-//   { Diamond: 'ace' }, { Diamond: 2 }, { Diamond: 3 }, { Diamond: 4 }, { Diamond: 5 }, { Diamond: 6 }, { Diamond: 7 }, { Diamond: 8 }, { Diamond: 9 }, { Diamond: 10 }, { Diamond: 'jack' }, { Diamond: 'queen' }, { Diamond: 'king' },
-//   { Heart: 'ace' }, { Heart: 2 }, { Heart: 3 }, { Heart: 4 }, { Heart: 5 }, { Heart: 6 }, { Heart: 7 }, { Heart: 8 }, { Heart: 9 }, { Heart: 10 }, { Heart: 'jack' }, { Heart: 'queen' }, { Heart: 'king' },
-//   { Club: 'ace' }, { Club: 2 }, { Club: 3 }, { Club: 4 }, { Club: 5 }, { Club: 6 }, { Club: 7 }, { Club: 8 }, { Club: 9 }, { Club: 10 }, { Club: 'jack' }, { Club: 'queen' }, { Club: 'king' },
-//   { Spade: 'ace' }, { Spade: 2 }, { Spade: 3 }, { Spade: 4 }, { Spade: 5 }, { Spade: 6 }, { Spade: 7 }, { Spade: 8 }, { Spade: 9 }, { Spade: 10 }, { Spade: 'jack' }, { Spade: 'queen' }, { Spade: 'king' }
-// ];
-
 function deal() {
   makeDeck();
   var shuffled = _.shuffle(deck);

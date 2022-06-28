4de690a6ff48e0f1c0b9845660fc61c7b699b4b5
Nolan Reda <nolan.reda@gmail.com>
2022-06-28 00:37:50 +0000

    changed equals

--- array-manipulation-3/src/equal.js
+++ array-manipulation-3/src/equal.js
@@ -1,32 +1,24 @@
 /* exported equal */
 
-// create a counter to track the first array position in the first array
-// loop through both arrays
-// if each one does not match when the loop counter is in the same place, return false
-// if they do match, increment the counter for the first array and continue the loop
-// if the end of the array is reached, return true
-// if the 2 arrays are not the same length, return false
-// if the arrays are empty, return true
+// create a counter to track the array position in the second array
+// loop through the first arrays
+// if the lengths don't match, return false
+// if one of the arrays reaches its end, return true,
+// if the arrays at each place match, increment the counter for the second array
+// if at any point the arrays at their counters don't match, return false
 
 function equal(first, second) {
   let s = 0;
-  if (first.length !== second.length) {
-    for (let f = 0; f < first.length; f++) {
-      if (first[f] === second[s]) {
-        s++;
-      } else if (s === second.length - 1) {
-        return true;
-      } else if (first[f] !== second[s]) {
-        return false;
-      }
+  for (let f = 0; f <= first.length; f++) {
+    if (first.length !== second.length) {
+      return false;
+    }
+    if (s === second.length) {
+      return true;
+    } else if (first[f] === second[s]) {
+      s++;
+    } else if (first[f] !== second[s]) {
+      return false;
     }
   }
 }
-
-// if (f === first.length - 1 && first[first.length === second[second.length]]) {
-//   return true;
-// } else if (first[f] === second[s]) {
-//   f++;
-// } else if (first[f] !== second[s]) {
-//   return false;
-// }

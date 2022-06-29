bbe3f90a593faf6ba6d8a80982ad5dd0c30a9809
Nolan Reda <nolan.reda@gmail.com>
2022-06-29 23:03:08 +0000

    finalized zip problem

--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,27 +1,16 @@
 /* exported zip */
 
 // create a new array to hold changed data from input array
-// create a counter var to track the first array
-// loop the second array
-// if one array reaches it's length, return the first created array
-// as each pair of index's is accessed create a new array and store them in first-second order
-// add each new array to the first created array
+// create a variable to repersent the Math.min of both input lengths
+// loop both arrays using the math variable
+// push an array of both the first and second inputs at i to the new array
+// return the array
 
 function zip(first, second) {
-  // var newArr = [];
-  // var f = 0;
-  // console.log(Math.min(first.length, second.length));
-  // for (let i = 0; i < second.length; i++) {
-  //   if (f === first.length || i === second.length) {
-  //     return newArr;
-  //   }
-  //   var newArr2 = [];
-  //   if (i === f) {
-  //     newArr2.push(first[f]);
-  //     newArr2.push(second[i]);
-  //     newArr.push(newArr2);
-  //     f++;
-  //   }
-  // }
-  // return newArr;
+  const newArr = [];
+  const short = Math.min(first.length, second.length);
+  for (let i = 0; i < short; i++) {
+    newArr.push([first[i], second[i]]);
+  }
+  return newArr;
 }

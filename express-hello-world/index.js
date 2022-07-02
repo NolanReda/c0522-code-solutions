115ca856830d797d9866dbd29bc3409fc482929a
Nolan Reda <nolan.reda@gmail.com>
2022-07-02 00:43:45 +0000

    end of day saves, not complete

--- express-hello-world/index.js
+++ express-hello-world/index.js
@@ -1,33 +1,9 @@
 const express = require('express');
 const app = express();
 
-const grades = {
-  12: {
-    id: 12,
-    name: 'Tim Berners-Lee',
-    course: 'HTML',
-    score: 95
-  },
-  47: {
-    id: 47,
-    name: 'Brendan Eich',
-    course: 'JavaScript',
-    score: 100
-  },
-  273: {
-    id: 273,
-    name: 'Forbes Lindsay',
-    course: 'JavaScript',
-    score: 92
-  }
-};
-
-app.get('/api/grades', (req, res) => {
-  const allGrades = [];
-  for (const key in grades) {
-    allGrades.push(grades[key]);
-  }
-  res.json(allGrades);
+app.use('/', (res, req) => {
+  // eslint-disable-next-line no-console
+  console.log(req.method);
 });
 
 app.listen(3000, () => {

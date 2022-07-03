c93f40e785becb779ca59eeade63133652527b95
Nolan Reda <nolan.reda@gmail.com>
2022-07-03 00:16:37 +0000

    completed post-json

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const grades = {};
+const grades = [];
 
 app.get('/api/grades', (req, res) => {
   const allGrades = [];
@@ -10,7 +10,16 @@ app.get('/api/grades', (req, res) => {
   res.json(allGrades);
 });
 
-express.json();
+app.use(express.json());
+
+const id = { id: 1 };
+
+app.post('/api/grades', (req, res) => {
+  Object.assign(req.body, id);
+  id.id++;
+  grades.push(req.body);
+  res.status(201).send(req.body);
+});
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console

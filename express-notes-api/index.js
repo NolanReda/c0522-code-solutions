bd795f852fd896f8e6df9a916fdec29ad83d0dcc
Nolan Reda <nolan.reda@gmail.com>
2022-07-05 22:30:47 +0000

    working error messages for post

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -15,7 +15,7 @@ app.get('/api/notes/:id', (req, res) => {
   const key = req.params.id;
   if (isNaN(Number(req.params.id)) === true) {
     res.status(400).send({
-      error: 'id but be a positive integer'
+      error: 'id must be a positive integer'
     });
   } else if ($json.notes[key]) {
     res.json($json.notes[key]);
@@ -31,9 +31,15 @@ app.use(express.json());
 app.post('/api/notes', (req, res) => {
   const newNote = req.body;
   const id = $json.nextId++;
-  newNote.id = id;
-  $json.notes[id] = newNote;
-  res.status(201).send(newNote);
+  if (!req.body.content) {
+    res.status(400).send({
+      error: 'content is a required field'
+    });
+  } else if (req.body.content) {
+    newNote.id = id;
+    $json.notes[id] = newNote;
+    res.status(201).send(newNote);
+  }
 });
 
 app.listen(3000, () => {

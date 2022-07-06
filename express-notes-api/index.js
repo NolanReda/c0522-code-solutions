ecd8064efe4530e24bf05c309665cb72962ae196
Nolan Reda <nolan.reda@gmail.com>
2022-07-06 01:00:46 +0000

    added put and completed challenge

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -78,10 +78,32 @@ app.delete('/api/notes/:id', (req, res) => {
   }
 });
 
-// app.put('/api/notes/:id', (req, res) => {
-//   const key = req.params.id;
-
-// });
+app.put('/api/notes/:id', (req, res) => {
+  const key = req.params.id;
+  const editNote = req.body.content;
+  if (isNaN(Number(req.params.id)) === true) {
+    res.status(400).send({
+      error: 'id must be a positive integer'
+    });
+  } else if (!req.body.content) {
+    res.status(400).send({
+      error: 'content is a required field'
+    });
+  } else if (req.body.content) {
+    $json.notes[key].content = editNote;
+    fs.writeFile('./data.json', JSON.stringify($json, null, 2), 'utf-8', err => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.log(err);
+        res.status(500).send({
+          error: 'An unexpected error occurred'
+        });
+      } else {
+        res.status(204).send(editNote);
+      }
+    });
+  }
+});
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console

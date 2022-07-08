c9f6a58d07619a1b97d82cf01a5aab5fde8dbfa0
Nolan Reda <nolan.reda@gmail.com>
2022-07-08 19:27:46 +0000

    completed optional section

--- node-fs-writefile/cp.js
+++ node-fs-writefile/cp.js
@@ -1,15 +1,11 @@
 const fs = require('fs');
 const pro2 = process.argv[2];
 
-let dataVar = '';
+const pro3 = process.argv[3];
 
 fs.readFile(pro2, 'utf8', (err, data) => {
   if (err) throw (err);
-  dataVar = data;
-});
-
-const pro3 = process.argv[3];
-
-fs.writeFile('./' + pro3, dataVar, 'utf8', err => {
-  if (err) throw err;
+  fs.writeFile('./' + pro3, data, 'utf8', err => {
+    if (err) throw err;
+  });
 });

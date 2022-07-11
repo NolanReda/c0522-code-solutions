870e5695370b84ed4a3e8a6f5b1abf323901d52c
Nolan Reda <nolan.reda@gmail.com>
2022-07-11 21:51:39 +0000

    complete array-reduce

---
+++ array-reduce/main.js
@@ -0,0 +1,47 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const account = [
+  { type: 'deposit', amount: 150 },
+  { type: 'deposit', amount: 20 },
+  { type: 'withdrawal', amount: 5 },
+  { type: 'deposit', amount: 100 },
+  { type: 'withdrawal', amount: 25 },
+  { type: 'withdrawal', amount: 60 }
+];
+
+const traits = [
+  { color: 'yellow' },
+  { type: 'electric' },
+  { name: 'pikachu' },
+  { level: 15 },
+  { trainer: 'ash' }
+];
+
+const initialValue = 0;
+const sum = numbers.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  initialValue
+);
+
+const product = numbers.reduce(
+  (previousValue, currentValue) => previousValue * currentValue
+);
+
+const balance = account.reduce(
+  (previousValue, currentValue) => {
+    if (currentValue.type === 'deposit') {
+      return previousValue + currentValue.amount;
+    } else {
+      return previousValue - currentValue.amount;
+    }
+  }, initialValue
+);
+
+const composite = traits.reduce(
+  (previousValue, currentValue) => {
+    const pokemon = {};
+    Object.assign(pokemon, previousValue);
+    Object.assign(pokemon, currentValue);
+    return pokemon;
+  }
+);

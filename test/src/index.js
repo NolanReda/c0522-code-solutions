646bc32a578c26818efd37d4e0df93147f5f497e
Nolan Reda <nolan.reda@gmail.com>
2022-07-13 22:46:55 +0000

    playing around

--- test/src/index.js
+++ test/src/index.js
@@ -1,9 +1,13 @@
 const data = require('./data');
 const Chart = require('../node_modules/chart.js/dist/chart');
 
+const img = new Image();
+img.src = './images/silver.png';
+
 const ctx = document.querySelector('#myChart');
+/* eslint-disable no-unused-vars */
 const myChart = new Chart(ctx, {
-  type: 'doughnut',
+  type: 'bar',
   data: {
     labels: ['Silvers', 'Gold Novas', 'MG', 'LE', 'Global Elite'],
     datasets: [{
@@ -16,37 +20,10 @@ const myChart = new Chart(ctx, {
         'rgb(28,28,28)',
         'rgb(193,178,107)'
       ],
-      borderColor: [
-        'rgb(136, 137, 139)',
-        'rgba(54, 162, 235)',
-        'rgba(255, 206, 86)',
-        'rgba(75, 192, 192)',
-        'rgba(153, 102, 255)',
-        'rgba(255, 159, 64)'
-      ],
-      borderWidth: 6
+      borderAlign: 'inner'
     }]
+  },
+  options: {
+    innerWidth: 20
   }
 });
-myChart();
-// const ctx = document.querySelector('#myChart').getContext('2d');
-// const myChart = new Chart(ctx, {
-//   type: 'bar',
-//   data: {
-//     labels: ['jan', 'feb', 'mar', 'apr', 'may', 'june', 'july'],
-//     datasets: [{
-//       label: 'my first dataset',
-//       backgroundColor: 'rgba(255, 99, 132)',
-//       borderColor: 'rgba(255, 99, 132)',
-//       borderWidth: 1,
-//       data: [12, 19, 3, 5, 2, 3]
-//     }]
-//   },
-//   options: {
-//     scales: {
-//       y: {
-//         beginAtZero: true
-//       }
-//     }
-//   }
-// });

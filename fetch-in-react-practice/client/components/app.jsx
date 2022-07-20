9b83b7a0a077ec3fd3c0ba3be10a4687aea4eb1b
Nolan Reda <nolan.reda@gmail.com>
2022-07-20 03:00:14 +0000

    finished POST

--- fetch-in-react-practice/client/components/app.jsx
+++ fetch-in-react-practice/client/components/app.jsx
@@ -41,12 +41,16 @@ export default class App extends React.Component {
     * TIP: Use Array.prototype.concat to create a new array containing the contents
     * of the old array, plus the object returned by the server.
     */
-  //   fetch('/api/todos', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: { task: JSON.stringify(newEntry), isComplete: false } })
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       const newEntry = this.state.todos.concat(data);
-  //       this.setState({ todos: newEntry });
-  //     });
+    fetch('/api/todos', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(newTodo)
+    })
+      .then(res => res.json())
+      .then(data => {
+        const newEntry = this.state.todos.concat(data);
+        this.setState({ todos: newEntry });
+      });
   }
 
   toggleCompleted(todoId) {

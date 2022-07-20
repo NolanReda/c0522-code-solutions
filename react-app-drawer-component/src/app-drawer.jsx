f222837caa4174bbea93135e11842372b7904093
Nolan Reda <nolan.reda@gmail.com>
2022-07-20 00:11:09 +0000

    working on it

--- react-app-drawer-component/src/app-drawer.jsx
+++ react-app-drawer-component/src/app-drawer.jsx
@@ -4,30 +4,27 @@ export default class AppDrawer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      drawerClassNames: 'drawer hidden',
-      containerClass: 'container'
+      isOpen: false
     };
     this.showMenu = this.showMenu.bind(this);
     this.hideMenu = this.hideMenu.bind(this);
   }
 
   showMenu(event) {
-    this.setState({ drawerClassNames: 'drawer', containerClass: 'container shadow' });
+    this.setState({ isOpen: true });
   }
 
   hideMenu(event) {
-    if (this.state.drawerClassNames === 'drawer') {
-      this.setState({ drawerClassNames: 'drawer hidden', containerClass: 'container' });
-    }
+    this.setState({ isOpen: false });
   }
 
   render() {
     return (
       <div>
-        <div onClick={this.hideMenu} className={this.state.containerClass}>
+        <div onClick={this.hideMenu} className={this.state.isOpen ? 'container shadow' : 'container' }>
           <img onClick={this.showMenu} className='menu-button' src='../src/img/menu.png' alt='menu button' />
         </div>
-        <div className={this.state.drawerClassNames}>
+        <div className={this.state.isOpen ? 'drawer' : 'drawer hidden'}>
           <h1>Choose a Game</h1>
           <h3 onClick={this.hideMenu}>The Legend of Zelda</h3>
           <h3 onClick={this.hideMenu}>A Link to the Past</h3>

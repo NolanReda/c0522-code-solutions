1b1ee32d26d664f290a45abeb642f035e2cf7cca
Nolan Reda <nolan.reda@gmail.com>
2022-08-30 01:15:42 +0000

    completed revisions

--- react-accordion-component/src/accordian.jsx
+++ react-accordion-component/src/accordian.jsx
@@ -13,28 +13,35 @@ export default class Accordian extends React.Component {
   }
 
   handleDrop(event) {
-    // console.log(event.target.id);
-
+    if (this.state.openTopic === event.target.textContent) {
+      this.setState({ openTopic: null });
+    } else {
+      this.setState({ openTopic: event.target.textContent });
+    }
   }
 
-  // createTopic(topic) {
-  //   return
-  //   })
-  // }
+  createTopic(topic) {
+    const { topics, openTopic } = this.state;
+    return topics.map(topic => {
+      return (
+        <div key={topic.name}>
+          <div onClick={this.handleDrop} className='dropdown'>
+            <h1>{topic.name}</h1>
+          </div>
+          <div className={topic.name === openTopic ? 'text-box' : 'hidden'}>
+          <p>{topic.text}</p>
+          </div>
+        </div>
+      );
+    });
+  }
 
   render() {
     return (
-      <div id='menu'>
-        <div onClick={this.handleDrop} id='html' className='container'>
-          <div className="drop-hide">
-            <h2>Hypertext Marckup Language</h2>
-          </div>
-          <div className="text-box">
-            <p>
-              {this.state.topics.name}
-            </p>
-          </div>
-        </div>
+      <div>
+        {
+          this.createTopic()
+        }
       </div>
     );
   }
